bbef3cb069fc94ad8e52385f5f0d30563ba4fde0
nimaghazanfari <ghazanfari_nima@yahoo.com>
2021-08-19 16:20:09 +0430

    home page modified

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -1,8 +1,42 @@
-
+import { Button, Col, Row } from 'react-bootstrap';
 
 const Home = () => {
     return (
-        <div>Home page</div>
+        <div className="home-page">
+            <Row>
+                <Col md={6}>
+                    <div className="desc-part">
+                        <p>
+                            Find the perfect solution
+                        </p>
+                        <p>
+                            For your requests
+                        </p>
+                    </div>
+                    <Row>
+                        <Col md={5} className="customer-part">
+                            <div className="d-grid gap-2">
+                                <Button variant="primary" size="lg">
+                                    For Customers
+                                </Button>
+                            </div>
+                        </Col>
+                        <Col md={5} className="expert-part">
+                            <div className="d-grid gap-2">
+                                <Button variant="success" size="lg">
+                                    For Experts
+                                </Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col md={6}>
+
+                    <img src="../coffee.jpg" />
+
+                </Col>
+            </Row>
+        </div>
     )
 }
 
