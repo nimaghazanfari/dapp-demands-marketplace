0ad6831564c7d39fb27f1b6b2f77a41e1ee88510
nimaghazanfari <ghazanfari_nima@yahoo.com>
2021-08-19 14:57:02 +0430

    login activated

---
+++ back-end/basic_data/mock_users.js
@@ -0,0 +1,18 @@
+const mockData = [{
+    username: 'admin',
+    password: 'admin',
+    token: 'edbd8cc7-e11c-4348-a0c9-be5f78e1cd48-6c4a112d-5feb-4baf-a97c-5771862266b1',
+    displayName: 'System Admin'
+},{
+    username: 'customer',
+    password: 'customer',
+    token: '4eff64ca-4baf-4348-b0ba-be5f78e1cd48-1c97d5eb-3e0d-4b2a-93d6-e09ffcaa5b9b',
+    displayName: 'John Smith'
+},{
+    username: 'customer2',
+    password: 'customer2',
+    token: '62012583-b038-4092-b0ba-f0b797eb9ae2-6c4a112d-5feb-4baf-a97c-5771862266b1',
+    displayName: 'Julia James'
+}];
+
+module.exports = mockData;
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+mockData = require('../basic_data/mock_users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/login', function (req, res, next) {
+
+  const { username, password } = req.body;
+  const result = mockData.find(a => {
+    return a.username == username && a.password == password
+  });
+
+  if (result != null) {
+    
+    const {
+      displayName,
+      token
+    } = result;
+    
+    res.send({ displayName, token });
+  }
+  else {
+    res.status(401).send('Invalid Username/Password!');
+  }
+
 });
 
 module.exports = router;
